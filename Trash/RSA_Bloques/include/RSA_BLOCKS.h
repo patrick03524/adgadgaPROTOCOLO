8dcee6406626a675d6ce6dabc1f7ae74299ac084
Patrick Márquez <dibujanteanime024@gmail.com>
2018-10-27 15:10:55 -0500

    SEXOANALSEXOANAKLALFFAGGSFOPINES

---
+++ Trash/RSA_Bloques/include/RSA_BLOCKS.h
@@ -0,0 +1,34 @@
+#ifndef RSA_BLOCKS_H
+#define RSA_BLOCKS_H
+
+#include "Func.h"
+
+class RSA_BLOCKS
+{
+    public:
+        RSA_BLOCKS(int);
+        RSA_BLOCKS(int,ZZ,ZZ);
+        string Encriptado(string);
+        string Desencriptado(string);
+        ZZ Resto_Chino(ZZ);
+        ///void menu();
+        string rellenado_0(int);
+        string rellenado_aux(ZZ,ZZ);
+        string rellenado_aux_esp(ZZ,ZZ);
+    private:
+        string alfabeto = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        ///string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        ZZ p;
+        ZZ q;
+        ZZ n;
+        ZZ phi;
+        ZZ e;
+        ZZ d;
+        ZZ e_pub;
+        ZZ n_pub;
+
+        int tam;
+        void generar_claves(int);
+};
+
+#endif // RSA_BLOCKS_H
---
+++ Trash/RSA_Bloques/main.cpp
@@ -0,0 +1,50 @@
+#include "RSA_BLOCKS.h"
+
+int main()
+{
+    /*cout << "-----------------------------------------------" <<endl;
+    cout << "-----------------------------------------------" <<endl;
+    cout << "------------------BIENVENIDO-------------------" <<endl;
+    cout << "-----------------------------------------------" <<endl;
+    cout << "----------------------RSA----------------------" <<endl;
+    cout << "-----------------------------------------------" <<endl;
+    cout << "----------------------CON----------------------" <<endl;
+    cout << "-----------------------------------------------" <<endl;
+    cout << "--------------------BLOQUES--------------------" <<endl;
+    cout << "-----------------------------------------------" <<endl;
+    system("pause");
+    system("cls");
+    unsigned short int op;
+    cout << "Ingrese la opcion que quiera realizar" <<endl;
+    cout << "1.- Instanciar y Asignar Claves" <<endl;
+    cout << "2.- Cifrado" <<endl;
+    cout << "3.- Descifrado" <<endl;
+    cout << "4.- Salir" <<endl;
+    cout << "Opcion: ";
+    cin >> op;
+    while(op!=4){
+        switch(op)
+        {
+        case 1:
+            ///instanciar xd
+        case 2:
+            ///cifrado
+            break;
+        case 3:
+            ///Descifrado"
+            break;
+        default:
+            cout << "jajajaja nel prro "<<endl;
+            break;
+        }
+        cout << "Ingrese la opcion otra ves: ";
+        cin >> op;
+    }
+    system("cls");
+    cout <<"Eligio la opcion 4"<<endl;
+    cout <<"Ta luegorl"<<endl;*/
+    ///RSA_BLOCKS cuack(8,ZZ(3),ZZ(1003));
+    RSA_BLOCKS cuack(8);
+    cuack.Encriptado("come here");
+    return 0;
+}
